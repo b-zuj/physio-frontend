a459bffcf01270ed5206eae8cbd2d525ff23ef15
maciej.glowacki <maciej.glowacki@appliedtechnology.se>
2020-11-23 17:58:57 +0100

    Redux setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import { login } from "./redux/actions/auth";
 
-function App() {
+import "./App.css";
+
+const App = (props) => {
+  console.log({ props });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>app</h1>
+      <button onClick={props.login}>Login</button>
     </div>
   );
-}
+};
+const mapStateToPros = (state) => {
+  console.log("asdbaksbdahksb", state);
+  return {
+    greatState: state.authReducer.something,
+    authState: state.authReducer,
+  };
+};
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  login: () => dispatch(login()),
+});
+export default connect(mapStateToPros, mapDispatchToProps)(App);
