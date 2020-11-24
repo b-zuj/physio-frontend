6835786b8a3671223388c30944a99e7e06681b02
maciej.glowacki <maciej.glowacki@appliedtechnology.se>
2020-11-24 14:03:35 +0100

    Create and connect basic views

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const Login = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Loginpage</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Login;
