3d3605989de32296b9325b106735e43843435e69
maciej.glowacki <maciej.glowacki@appliedtechnology.se>
2020-11-24 14:03:35 +0100

    Create and connect basic views

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Redirect, Route, Switch, useHistory } from "react-router-dom";
+
 import * as actions from "./redux/actions/auth";
-import Layout from './components/Layout/Layout';
+import {
+  Home,
+  Login,
+  Signup,
+  Dashboard,
+  Session,
+  Account,
+  About,
+} from "./pages";
 
 import "./App.css";
-// testing component. Change it later for real Components
-const Comp = (props) => {
-  console.log(props);
-  return <h1>text</h1>;
-};
 
 const App = (props) => {
   const history = useHistory();
   const { accType, isAuth } = props;
   let routes = (
     <>
-      <Route path="/login" component={(props) => <Comp {...props} />} />
-      <Route path="/signup" component={(props) => <Comp {...props} />} />
+      <Route path="/login" component={(props) => <Login {...props} />} />
+      <Route path="/signup" component={(props) => <Signup {...props} />} />
     </>
   );
 
@@ -26,16 +30,16 @@ const App = (props) => {
       <>
         <Route
           path="/dashboard"
-          component={(props) => <Comp accType={accType} {...props} />}
+          component={(props) => <Dashboard accType={accType} {...props} />}
         />
         <Route
           path="/account"
-          component={(props) => <Comp accType={accType} {...props} />}
+          component={(props) => <Account accType={accType} {...props} />}
         />
         <Route
           exact
           path="/session"
-          component={(props) => <Comp accType={accType} {...props} />}
+          component={(props) => <Session accType={accType} {...props} />}
         />
       </>
     );
@@ -50,9 +54,9 @@ const App = (props) => {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={(props) => <Comp {...props} />} />
+        <Route exact path="/" component={(props) => <Home {...props} />} />
         {routes}
-        <Route path="/about" component={(props) => <Comp {...props} />} />
+        <Route path="/about" component={(props) => <About {...props} />} />
         <Redirect to="/" />
       </Switch>
       <h1>app</h1>
