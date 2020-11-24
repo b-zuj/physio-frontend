0e5efac35d38d68f284deb1de11f57e86bcca596
mountwebs <mountwebs@gmail.com>
2020-11-24 11:45:39 +0100

    Started creating layout with footer and header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from "react";
-import { connect } from "react-redux";
-import { login } from "./redux/actions/auth";
+import React from 'react';
+import { connect } from 'react-redux';
+import { login } from './redux/actions/auth';
+import Layout from './components/Layout/Layout';
 
-import "./App.css";
+import './App.css';
 
 const App = (props) => {
   console.log({ props });
   return (
     <div className="App">
-      <h1>app</h1>
-      <button onClick={props.login}>Login</button>
+      <Layout>
+        <h1>app</h1>
+        <button onClick={props.login}>Login</button>
+      </Layout>
     </div>
   );
 };
 const mapStateToPros = (state) => {
-  console.log("asdbaksbdahksb", state);
+  console.log('asdbaksbdahksb', state);
   return {
     greatState: state.authReducer.something,
     authState: state.authReducer,
---
+++ src/components/Layout/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Layout.module.css';
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbarContainer}>
+      <div className={styles.navbar}>
+        <ul>
+          <li>
+            <a href="/signup">Signup</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/dashboard">Dashboard</a>
+          </li>
+          <li>
+            <a href="/account">Account</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
