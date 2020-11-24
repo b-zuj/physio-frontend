f15a14881eee9aa40bda3be7aa0135b18c240a21
maciej.glowacki <maciej.glowacki@appliedtechnology.se>
2020-11-24 14:34:30 +0100

    Little styles in Main and login btn

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Footer from './Footer/Footer';
-import Navbar from './Navbar/Navbar';
+import React from "react";
+import Footer from "./Footer/Footer";
+import Navbar from "./Navbar/Navbar";
+import styles from "./Layout.module.css";
 
 const Layout = (props) => {
   return (
-    <>
+    <div className={styles.app}>
       <Navbar />
-      {props.children}
+      <main className={styles.main}>{props.children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+
 import Layout from "../components/Layout/Layout";
 
-const Login = () => {
+import * as actions from "../redux/actions/auth";
+
+const Login = (props) => {
+  const history = useHistory();
+  const loginHandler = () => {
+    props.login();
+    history.push("/dashboard");
+  };
   return (
     <div>
       <Layout>
         <h1>Loginpage</h1>
+        <button onClick={loginHandler}>Login</button>
       </Layout>
     </div>
   );
 };
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: () => dispatch(actions.login()),
+  logout: () => dispatch(actions.logout()),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
