5552670e9b7efd62b1b24b3a18c1b5c44b856ea8
mountwebs <mountwebs@gmail.com>
2020-11-25 21:18:05 +0100

    Added mock db

---
+++ src/mock/mockDB.js
@@ -0,0 +1,81 @@
+export const db = {
+  clients: [
+    {
+      id: '213',
+      name: 'Adam Kowalsk',
+      email: 'asdf@sdf.no',
+      status: 'Pending',
+      comments: '',
+    },
+    {
+      id: '34r',
+      name: 'Jane Doe',
+      email: 'assdfdf@sdf.no',
+      status: 'Active',
+      comments:
+        'Test ødflsk jdsflk jdsf lkjdfsøl kjdf lkj dflkjsdløkfjasdlfkjsdflk jd fslkj sølkj',
+      sessions: [
+        {
+          title: 'Lower back training',
+          id: 'h8fa',
+          exercices: [
+            {
+              id: 'oisuf98',
+              title: 'Bridge  - begginer level',
+              desciption:
+                'Do 3 series of 15 repetitions. Hold 10 sec in the posision.',
+            },
+            {
+              id: 'ouh8',
+              title: 'Knee-to-chest stretches',
+              desciption:
+                'Do 3 series of 15 repetitions. Hold 10 sec in the posision.',
+            },
+            {
+              id: 'ihf8',
+              title: 'Lower back stretches',
+              desciption:
+                'Do 3 series of 15 repetitions. Hold 10 sec in the posision.',
+            },
+          ],
+        },
+        { title: 'Everyday exercices', id: 'p98uawf' },
+      ],
+    },
+    {
+      id: '23rfv',
+      name: 'Pia Kekkajarvi',
+      email: 'asdf@sdf.no',
+      status: 'Active',
+      comments:
+        'Test ødflsk jdsflk jdsf lkjdfsøl kjdf lkj dflkjsdløkfjasdlfkjsdflk jd fslkj sølkj',
+      sessions: [
+        {
+          title: 'Lower back training',
+          id: 'h8fa',
+          exercices: [
+            {
+              id: 'oisuf98',
+              title: 'Bridge  - begginer level',
+              desciption:
+                'Do 3 series of 15 repetitions. Hold 10 sec in the posision.',
+            },
+            {
+              id: 'ouh8',
+              title: 'Knee-to-chest stretches',
+              desciption:
+                'Do 3 series of 15 repetitions. Hold 10 sec in the posision.',
+            },
+            {
+              id: 'ihf8',
+              title: 'Lower back stretches',
+              desciption:
+                'Do 3 series of 15 repetitions. Hold 10 sec in the posision.',
+            },
+          ],
+        },
+        { title: 'Everyday exercices', id: 'p98uawf' },
+      ],
+    },
+  ],
+};
