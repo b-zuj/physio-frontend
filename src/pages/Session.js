14131c26b621433b67dff6a0c6f69034d75fa4f2
mountwebs <mountwebs@gmail.com>
2020-11-26 10:02:26 +0100

    added basick view session

--- src/pages/Session.js
+++ src/pages/Session.js
@@ -3,18 +3,40 @@ import Layout from '../components/Layout/Layout';
 import { useParams } from 'react-router-dom';
 import { db } from '../mock/mockDB';
 import ExcerciseList from '../components/session/ExerciseList';
+import { Link } from 'react-router-dom';
 
 const Session = () => {
   let { id } = useParams();
 
   const session = db.sessions.find((element) => element.id === id);
 
+  const renderSession = () => {
+    if (!session) {
+      return 'No matching session';
+    }
+
+    const description = (
+      <>
+        <h3>Description</h3>
+        <p>{session.description}</p>
+      </>
+    );
+
+    return (
+      <>
+        <h1>Session: {session.title}</h1>
+        {session.description && description}
+        <ExcerciseList exercises={session.exercises} />
+        <Link to={`/session/${session.id}/edit`}>
+          <button type="button">Edit session</button>
+        </Link>
+      </>
+    );
+  };
+
   return (
     <div>
-      <Layout>
-        <h1>Session: {session.title}</h1>
-        <ExcerciseList exercises={session.exercises} />
-      </Layout>
+      <Layout>{renderSession()}</Layout>
     </div>
   );
 };
