75046c2c48fc81bc8536384a2a4202b8a994eaec
mountwebs <mountwebs@gmail.com>
2020-11-28 16:23:21 +0100

    removed console.log

--- src/redux/actions/invitation.js
+++ src/redux/actions/invitation.js
@@ -4,7 +4,6 @@ import * as authActions from './auth';
 export function createInvitation(invitationData) {
   return async (dispatch) => {
     const response = await axios.post(`/invitations`, invitationData);
-    console.log('invitation', response);
     if (response.status === 200) {
       dispatch(authActions.tryToAutoLog());
     }
