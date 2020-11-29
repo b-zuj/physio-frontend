ebcf287fb1dca75619071f8304fa32bc56954006
mountwebs <mountwebs@gmail.com>
2020-11-29 11:53:03 +0100

    updated to changes in api

--- src/components/Pro/Dashboard/PendingList.js
+++ src/components/Pro/Dashboard/PendingList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import styles from './PendingList.module.css';
 
 const PendingList = (props) => {
-  const { emails } = props;
+  const { invitations } = props;
 
-  const clientEntries = emails.map((email) => (
+  const clientEntries = invitations.map((invitation) => (
     <>
-      <li>
-        <a href={`mailto:${email}`}>{email}</a>
+      <li key={invitation._id}>
+        <a href={`mailto:${invitation.email}`}>{invitation.email}</a>
       </li>
     </>
     // <Client key={client._id} className={styles.client} client={client} />
@@ -16,7 +16,7 @@ const PendingList = (props) => {
   return (
     <div className={styles.pendingList}>
       <h3>Pending clients</h3>
-      {emails.length > 0 ? <ul>{clientEntries}</ul> : 'No pending clients'}
+      {invitations.length > 0 ? <ul>{clientEntries}</ul> : 'No pending clients'}
     </div>
   );
 };
