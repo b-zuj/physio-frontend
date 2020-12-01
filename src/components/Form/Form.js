32bc9f3e159ab0206213a582872a12fd5e061c08
maciej.glowacki <maciej.glowacki@appliedtechnology.se>
2020-12-01 11:40:44 +0100

    Submit form works on Enter

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
 import Input from '../Input/Input';
+import Button from '../Button/Button';
 import classes from './Form.module.css';
 
 import { objToArray } from '../../utils/formHelpers';
 
-const Form = (props) => {
-  const { submitHandler, changedHandler, formElements } = props;
+const Form = props => {
+  const {
+    submitHandler,
+    changedHandler,
+    formElements,
+    addedClassName,
+    submitBtn,
+  } = props;
   const formElementsArray = objToArray(formElements);
+  const attachedClasses = [classes.basic];
+  switch (addedClassName) {
+    case 'login':
+      attachedClasses = [...attachedClasses, addedClassName];
+      break;
+
+    default:
+      break;
+  }
   return (
     <form className={classes.Form} onSubmit={submitHandler}>
-      {formElementsArray.map((el) => (
+      {formElementsArray.map(el => (
         <Input
           key={el.id}
           elementType={el.config.elementType}
@@ -22,6 +38,7 @@ const Form = (props) => {
           options={el.config.options}
         />
       ))}
+      <Button type="submit" action={submitHandler} actionStyle="hidden" />
     </form>
   );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
+import { changedHandler, createFormData } from '../utils';
+
 import Layout from '../components/Layout/Layout';
-import Input from '../components/Input/Input';
+import Form from '../components/Form/Form';
 import Button from '../components/Button/Button';
 
-import checkValidity from '../utils/formValidation';
 import * as authActions from '../redux/actions/auth';
 import * as errorsActions from '../redux/actions/errors';
 
-const Login = (props) => {
+const Login = props => {
   const [formElements, setFormElements] = useState({
     email: {
       elementType: 'input',
@@ -36,79 +37,40 @@ const Login = (props) => {
   });
 
   // Submit:
-  const submitHandler = (e) => {
+  const submitHandler = e => {
     e.preventDefault();
-
-    const formData = {};
-    for (let key in formElements) {
-      formData[key] = formElements[key].value;
+    try {
+      const formData = createFormData(formElements, null, 'login');
+      props.cleanFormError();
+      props.login(formData);
+    } catch (error) {
+      props.addFormError(error);
     }
-
-    const errors = checkValidity(formData, 'login');
-
-    if (Object.keys(errors).length !== 0) {
-      return props.addFormError('Invalid email or password. Please try again.');
-    }
-    props.cleanFormError();
-    props.login(formData);
   };
-
-  // Handle change value
-  const updateState = (identifier, targetToUpdate, value) => {
-    setFormElements((prevState) => ({
-      ...prevState,
-      [identifier]: {
-        ...prevState[identifier],
-        [targetToUpdate]: value,
-      },
-    }));
-  };
-
-  const changedHandler = (e) => {
-    updateState(e.target.name, 'value', e.target.value);
-  };
-
-  // Map over object to create array
-  const formElementsArray = [];
-
-  for (let key in formElements) {
-    formElementsArray.push({
-      id: key,
-      config: formElements[key],
-    });
-  }
 
   return (
     <div>
       <Layout>
         <h1>Login</h1>
-        {props.errorMessage && <span>{props.errorMessage}</span>}
-        <form onSubmit={submitHandler}>
-          {formElementsArray.map((el) => (
-            <Input
-              key={el.id}
-              elementType={el.config.elementType}
-              elementConfig={el.config.elementConfig}
-              value={el.config.value}
-              changed={changedHandler}
-              label={el.config.label}
-              invalid={el.config.error}
-            />
-          ))}
-          <Button type="submit" action={submitHandler} actionStyle="create">
-            Login
-          </Button>
-        </form>
+        <Form
+          submitHandler={submitHandler}
+          changedHandler={e => changedHandler(e, 'value', setFormElements)}
+          formElements={formElements}
+          submitBtn
+        />
+        <Button type="submit" action={submitHandler} actionStyle="create">
+          Login
+        </Button>
       </Layout>
     </div>
   );
 };
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   errorMessage: state.authReducer.error,
 });
-const mapDispatchToProps = (dispatch) => ({
-  login: (loginCredentials) => dispatch(authActions.login(loginCredentials)),
-  addFormError: (message) => dispatch(errorsActions.addFormError(message)),
+const mapDispatchToProps = dispatch => ({
+  login: loginCredentials => dispatch(authActions.login(loginCredentials)),
+  addFormError: message => dispatch(errorsActions.addFormError(message)),
   cleanFormError: () => dispatch(errorsActions.cleanFormError()),
 });
 
