1ee0fd24c0632296390a8eafa1652809215dd0ee
mountwebs <mountwebs@gmail.com>
2020-12-02 16:03:06 +0100

    added logo clickability

--- src/components/shared/Logo/Logo.js
+++ src/components/shared/Logo/Logo.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import classes from './Logo.module.css';
+import { Link } from 'react-router-dom';
 
-import logo from '../../../images/logo_transparent_crop2.png';
 import SvgLogo from './SvgLogo';
 
-const Logo = ({ comp }) => {
+const Logo = ({ comp, link }) => {
   let attachedClasses = classes.Logo;
 
   if (comp) {
     attachedClasses = [classes.Logo, classes[comp]].join(' ');
   }
-  console.log(attachedClasses);
+  if (link) {
+    return (
+      <div className={attachedClasses}>
+        <Link to={link}>
+          <SvgLogo />
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div className={attachedClasses}>
       <SvgLogo />
