f959308201b78294c0fe5efdb4ab331065b944ee
barbara zujewska <barbara@salt-admins-mbp.lan>
2020-12-02 15:44:23 +0100

    styling about v1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,57 +1,86 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
 import { connect } from 'react-redux';
-import styles from './styles/textContent.module.css';
+import styles from './styles/About.module.css';
+import meditation from '../images/meditation.png';
+import cycling from '../images/cycling.png';
+import paperPlane from '../images/paperPlane.png';
+import logo from '../images/logo.svg';
+import email from '../images/email.svg';
 
 const About = ({ isAuth }) => {
   console.log(isAuth);
   const notLoggedInContent = (
     <>
+      <h3 className={styles.h3}>Are you interested in using our services?</h3>
       <p>
-        <b>Are you interested in using our services?</b>
-      </p>
-      <p>
-        For physiotherapists: Please contact us and request a demo or ask us any
+        <b>For physiotherapists:</b><br />
+        Please contact us and request a demo or ask us any
         questions.
       </p>
       <p>
-        For clients: Contact us for any questions or tell your physiotherapist
+      <b>For clients:</b><br />
+        Contact us for any questions or tell your physiotherapist
         about us.
       </p>
     </>
   );
 
   return (
-    <div className={styles.textContent}>
-      <Layout type="text">
-        <h1>About</h1>
-        <p>
-          PyshIO is an app for pysiotherapists and their clients. Our aim is to
-          make it easier for pysiotherapists to share their services to clients
-          in a digital world.
-        </p>
-        <h3>Our mission</h3>
-        <p>
-          Due to the global pandemic many countries impose restrictions on
-          physical contact, functioning of training and medical facilities.This
-          makes it difficult for many to keep in shape, but also receive
-          necessary treatments such as physiotherapy. Our app attempts to solve
-          this issue. It allows personal trainers and physiotherapists to
-          continue providing their services and follow up with their customers
-          regardless of the local restrictions.
-        </p>
-        {!isAuth && notLoggedInContent}
-        <h3>Contact</h3>
-        <p>
-          <a href="mailto:contact@phys.io">contact@phys.io</a>
-        </p>
-        <h3>Work with us</h3>
+    <Layout type="text">
+      <header className={styles.header}>
+        <img className={styles.logo} src={logo} alt="logo" />
+      </header>
+      
+      <article className={styles.article}>
+        
+        <img className={styles.ilustration}  src={cycling} alt="cycling" />
+        
+        <section className={styles.section}>
+          <h1>About</h1>
+          <p>
+            We are Loopers, a group of dedicated web developers embarked on a mission to build
+            PhysIO. The first app dedicated to solve communication issues between 
+            physiotherapists and their clients. <br />
+            <br />
+            <b>Let's take physiotherapy into the digital world.</b>
+          </p>
+        </section>
+
+        <img className={styles.ilustration} src={meditation} alt="meditation" />
+        
+        <section className={styles.section}>
+          <h3 className={styles.h3}>Our mission</h3>
+          <p>
+            Due to the global pandemic many countries impose restrictions on
+            physical contact, functioning of training and medical facilities. This
+            makes it difficult for many to keep in shape, but also receive
+            necessary treatments such as physiotherapy. Our app attempts to solve
+            this issue. It allows personal trainers and physiotherapists to
+            continue providing their services and follow up with their customers
+            regardless of where you are and what are the local restrictions.
+          </p>
+          <br /><br /><br />
+        </section>
+
+        <img className={styles.ilustration} src={paperPlane} alt="paper plane" />
+        
+        <section className={styles.section}>
+          <h1>Contact</h1>
+          <br />
+          {!isAuth && notLoggedInContent}
+        </section>
+        <div className={styles.contactDiv}>
+            <a href="mailto:contact@phys.io"><img className={styles.emailImg} src={email} alt="envelop"/><br />
+            contact@phys.io</a>
+          </div>
+        <h3 className={styles.h3}>Work with us</h3>
         <p>
           Introduce yourself -{' '}
           <a href="mailto:career@phys.io">career@phys.io</a>
         </p>
-      </Layout>
-    </div>
+      </article>
+    </Layout>
   );
 };
 
