25a767d5b3d61f916934ab66e3cd968376d3c22e
mountwebs <mountwebs@gmail.com>
2020-12-03 12:28:33 +0100

    added main colors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import './colorThemes/default.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import store from "./redux/store";
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,7 +18,7 @@ ReactDOM.render(
       </Router>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
