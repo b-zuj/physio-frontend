3a743b336287b7dc473813c3c2cdf46fb637dba9
mountwebs <mountwebs@gmail.com>
2020-12-03 11:03:56 +0100

    used form component on invite page. added description to form comp

--- src/pages/Invite.js
+++ src/pages/Invite.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import Layout from '../components/Layout/Layout';
-import Input from '../components/Input/Input';
-import checkValidity from '../utils/formValidation';
+import Form from '../components/Form/Form';
+import { changedHandler, createFormData } from '../utils';
+
 import * as errorsActions from '../redux/actions/errors';
 import * as invitationActions from '../redux/actions/invitation';
-import Button from '../components/Button/Button';
 
 const Invite = (props) => {
+  const { addFormError, cleanFormError, createInvitation } = props;
+  const [feedback, setFeedback] = useState('');
   const [formElements, setFormElements] = useState({
     email: {
       elementType: 'input',
@@ -22,80 +24,56 @@ const Invite = (props) => {
       value: '',
     },
   });
-  const [feedback, setFeedback] = useState('');
+
+  const cleanForm = () => {
+    setFormElements({
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Email',
+          name: 'email',
+          id: 'email',
+        },
+        label: '',
+        value: '',
+      },
+    });
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
-
-    const formData = {};
-    for (let key in formElements) {
-      formData[key] = formElements[key].value;
+    try {
+      const formData = createFormData(formElements, null, 'invite');
+      cleanFormError();
+      console.log(formData);
+      setFeedback(`Invitation sendt to ${formData.email} .`);
+      createInvitation(formData);
+      cleanForm();
+    } catch (error) {
+      addFormError(error);
     }
-
-    const errors = checkValidity(formData, 'invite');
-    if (Object.keys(errors).length !== 0) {
-      return props.addFormError('Invalid email. Please try again.');
-    }
-    props.cleanFormError();
-    props.createInvitation(formData);
-    setFeedback(`Invitation sendt to ${formData.email} .`);
   };
-
-  const updateState = (identifier, targetToUpdate, value) => {
-    setFormElements((prevState) => ({
-      ...prevState,
-      [identifier]: {
-        ...prevState[identifier],
-        [targetToUpdate]: value,
-      },
-    }));
-  };
-
-  const changedHandler = (e) => {
-    updateState(e.target.name, 'value', e.target.value);
-  };
-
-  // Map over object to create array
-  const formElementsArray = [];
-
-  for (let key in formElements) {
-    formElementsArray.push({
-      id: key,
-      config: formElements[key],
-    });
-  }
 
   return (
     <div>
       <Layout>
-        <h1>Invite</h1>
-        <p>Invite a new client to join physio:</p>
-        {props.errorMessage && <span>{props.errorMessage}</span>}
-        <form>
-          {formElementsArray.map((el) => (
-            <Input
-              key={el.id}
-              elementType={el.config.elementType}
-              elementConfig={el.config.elementConfig}
-              value={el.config.value}
-              changed={changedHandler}
-              label={el.config.label}
-              invalid={el.config.error}
-            />
-          ))}
-          <Button type="submit" action={submitHandler} actionStyle="create">
-            Send invitation
-          </Button>
-          {<p>{feedback}</p>}
-        </form>
+        <Form
+          submitHandler={submitHandler}
+          changedHandler={(e) => changedHandler(e, 'value', setFormElements)}
+          formElements={formElements}
+          heading="Invite"
+          addedClassName="invite"
+          btn={'Invite'}
+          description="Invite a new client to join physio:"
+          feedback={feedback}
+        >
+          <p>{feedback}</p>
+        </Form>
       </Layout>
     </div>
   );
 };
-
-const mapStateToProps = (state) => ({
-  errorMessage: state.authReducer.error,
-});
 
 const mapDispatchToProps = (dispatch) => ({
   addFormError: (message) => dispatch(errorsActions.addFormError(message)),
@@ -104,4 +82,4 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch(invitationActions.createInvitation(data)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Invite);
+export default connect(null, mapDispatchToProps)(Invite);
