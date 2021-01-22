1c61807599d2f07c8c5ac8955387a4f308159a37
Maciej Glowacki <m.glowacki01@gmail.com>
2021-01-22 14:53:35 +0100

    lockfile update and baseURL changed to production

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,27 +2,27 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const customAxios = axios.create({
-  baseURL: 'http://localhost:4000',
+	baseURL: 'https://physio-app-server.herokuapp.com',
 });
 
-customAxios.interceptors.request.use(req => {
-  const token = Cookies.get('auth');
-  req.headers.Authorization = `Bearer ${token}`;
-  return req;
+customAxios.interceptors.request.use((req) => {
+	const token = Cookies.get('auth');
+	req.headers.Authorization = `Bearer ${token}`;
+	return req;
 });
 
 customAxios.interceptors.response.use(
-  res => {
-    if (res.data?.token) {
-      Cookies.set('auth', res.data.token);
-    }
-    return res;
-  },
-  error => {
-    const customErrorMessage = error.response?.data?.error;
-    console.log({ customErrorMessage });
-    throw customErrorMessage ? customErrorMessage : { error };
-  }
+	(res) => {
+		if (res.data?.token) {
+			Cookies.set('auth', res.data.token);
+		}
+		return res;
+	},
+	(error) => {
+		const customErrorMessage = error.response?.data?.error;
+		console.log({ customErrorMessage });
+		throw customErrorMessage ? customErrorMessage : { error };
+	}
 );
 
 export default customAxios;
